61aa0b05fd4b67736cfc95f1cba811c4c9b40e39
Daniel Martinez Dacol <86168700+daniel-matinez@users.noreply.github.com>
2022-09-22 11:15:13 -0300

    Update script.js

--- CompGraficaDMD/script.js
+++ CompGraficaDMD/script.js
@@ -1,31 +1,22 @@
 "use strict"
 var canvas = document.getElementById("tela");
-var ctx = canvas.getContext("2d");
-var x=200,y=100,larg=300,alt=150;
+var gl = canvas.getContext("webgl");
+
+function main(){
+if(!gl){
+    console.error("Navegador não suporta WebGL");
+    return;
+}
+gl.clearColor(0,0,0,1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+var render = new Render("tela");
 
 function desenhar(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle = "rgb(255, 128, 0)";
+    render.draw();
     requestAnimationFrame(desenhar);
-    //CIRCULO:
-    ctx.fillStyle="#7f0000";
-    ctx.beginPath();
-    ctx.arc(320,240,230,20,Math.PI*2,true);
-    ctx.closePath();
-    ctx.fill();
-    //risco:
-    ctx.beginPath();
-    ctx.moveTo(320,220);
-    ctx.lineTo(300,300);
-    ctx.lineTo(200,300);
-    ctx.lineTo(400,-490);
-    ctx.lineTo(300,190);
-    ctx.stroke();
-
 }
 
 requestAnimationFrame(desenhar);
-
-document.onkeydown = function (evt) {
-    x += 5;
-}
+main();
